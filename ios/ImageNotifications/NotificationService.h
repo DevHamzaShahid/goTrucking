4c7efb436b7ddb5423e634e488e8ed7be34579a6
elevendev11 <eng.hamza97@gmail.com>
2023-08-31 09:25:12 +0500

    Notifications and map optimizations

---
+++ ios/ImageNotifications/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotifications
+//
+//  Created by Eleven Dev on 29/08/2023.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
